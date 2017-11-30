4d9deb5a531401afb860a7c2750ae216379cc317
Rose Cox <rose@purpleprodigy.com>
2017-11-30 15:29:26 +1000

    Remove unneeded comments

--- src/views/summary.php
+++ src/views/summary.php
@@ -1,11 +1,3 @@
-<!--  --><?php
-//  $summary_id = '';
-//  if ( ) {
-//	  $summary_id = '$post_id';
-//  } else {
-//	  $summary_id = '$accommodation_id';
-//  }
-//  ?>
     <a class="button" href="<?php echo esc_url( get_post_meta( $post_id, 'booking_system_url', true ) ); ?>">BOOK NOW</a>
 </p>
 </div>
@@ -14,14 +6,3 @@
 	<li><i class="fa fa-bed"></i><?php esc_html_e( get_post_meta( $post_id, 'beds', true ) ); ?></li>
 	<li><i class="fa fa-usd"></i><?php esc_html_e( get_post_meta( $post_id, 'price', true ) ); ?></li>
 </ul>
-
-<!--    <a class="button"-->
-<!--       href="--><?php //echo esc_url( get_post_meta( $accommodation_id, 'booking_system_url', true ) ); ?><!--">BOOK-->
-<!--        NOW</a>-->
-<!--    </p>-->
-<!--    <ul class="summary" itemprop="amenityFeature">-->
-<!--        <li><i class="fa fa-male"></i>--><?php //esc_html_e( get_post_meta( $accommodation_id, 'sleeps', true ) ); ?>
-<!--        </li>-->
-<!--        <li><i class="fa fa-bed"></i>--><?php //esc_html_e( get_post_meta( $accommodation_id, 'beds', true ) ); ?><!--</li>-->
-<!--        <li><i class="fa fa-usd"></i>--><?php //esc_html_e( get_post_meta( $accommodation_id, 'price', true ) ); ?><!--</li>-->
-<!--    </ul>-->
