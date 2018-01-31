ea90d0db470aeddd53389b057ad5aeb324af8c74
Rose Cox <rose@purpleprodigy.com>
2018-01-31 16:16:22 +1000

    Add content to taxonomy archive, update public arg in config + small fixes

--- src/template/taxonomy-accommodation-type.php
+++ src/template/taxonomy-accommodation-type.php
@@ -10,3 +10,21 @@
  */
 
 namespace PurpleProdigy\Accommodation\Template;
+
+add_action( 'genesis_entry_content', __NAMESPACE__ . '\render_accommodation_image', 8 );
+
+add_action( 'genesis_entry_content', __NAMESPACE__ . '\render_summary', 11 );
+/**
+ * Render the footer which includes the booking button and summary.
+ *
+ * @since 1.0.0
+ *
+ * @return void
+ */
+function render_summary() {
+	$accommodation_id = (int) get_the_ID();
+
+	require dirname( __DIR__ ) . '/views/summary.php';
+}
+
+genesis();
